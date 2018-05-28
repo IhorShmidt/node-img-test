cc25a854f28bdc62713e29291e3290200cc9df5e
IhorShmidt <ihor.shmidt@techmagic.co>
2018-05-28 16:28:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,138 @@
+const fs = require('fs');
+const request = require('request');
+const _ = require('lodash');
+const Promise = require('bluebird');
+
+class Scrapper {
+
+    constructor(dir, data) {
+        console.log('Initialization... ');
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        this.data = data;
+    }
+
+    makeMagic() {
+        return Promise.resolve()
+            .then(this.mergeLikedPosts.bind(this))
+            .then(this.prepareImages.bind(this))
+            .then(this.saveInJson.bind(this))
+            // .then(this.saveImages.bind(this))
+            .then(this.finish)
+    }
+
+    saveImages(imageArray) {
+        return Promise.reduce(imageArray, (accum, image, index) => {
+            const percentage = ((index / imageArray.length) * 100).toFixed(1);
+            return new Promise((resolve) => {
+
+                console.log(`Processing... ${image.name.replace(ops.dirName + '/', '')} -- ${percentage} %`);
+                return this.saveImage(image.url, image.name, () => {
+                    return resolve(image);
+                });
+            })
+                .then((img) => {
+                    accum.push(img);
+                    return accum;
+                });
+        }, [])
+            .then((result) => result);
+    }
+
+    saveImage(uri, photoName, callback) {
+        return request.head(uri, (err, res, body) => {
+            return request(uri).pipe(fs.createWriteStream(photoName)).on('close', callback);
+        });
+    }
+
+    mergeLikedPosts() {
+        console.log('Preparing posts...');
+        const wholeArray = this.data.liked.newValue.items.concat(this.data.liked.oldValue.items);
+        const allData = _.uniqBy(wholeArray, 'code');
+        console.log('Posts prepared, total: ', allData.length, ' items');
+        return allData;
+    }
+
+    getPhotoName(image) {
+        let fullName = image.caption ? image.caption.user.full_name : image.user.full_name;
+        fullName = fullName.replace('/', '');
+        return `${ops.dirName}/${fullName}_${this.randomString()}.jpeg`;
+    }
+
+    randomString() {
+        let text = '';
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        _.times(5, () => text += possible.charAt(Math.floor(Math.random() * possible.length)));
+        return text;
+    }
+
+
+    prepareImages(data) {
+        let prepared = [];
+
+        _.each(data, (processingImage) => {
+
+            if (processingImage.image_versions2) {
+
+                const imageObject = this.imageInterface(processingImage);
+                return imageObject && prepared.push(imageObject);
+
+            }
+
+            if (!processingImage.image_versions2 && processingImage.carousel_media) {
+                const carouselArray = [];
+
+                _.each(processingImage.carousel_media, (item) => {
+                    const imageObject = this.imageInterface(item, processingImage);
+                    imageObject && carouselArray.push(imageObject);
+                });
+
+                prepared = prepared.concat(...carouselArray);
+                return prepared;
+            }
+
+        });
+        console.log(`Images prepared, total: ${prepared.length}`);
+        return prepared;
+    }
+
+    imageInterface(image, processingImage) {
+        const imageUrl = this.getImageUrl(image.image_versions2);
+        return {
+            url: imageUrl,
+            id: processingImage && processingImage.code || image.code,
+            name: this.getPhotoName(processingImage || image)
+        };
+    }
+
+    getImageUrl(imageVersions) {
+        return imageVersions.candidates[0].url;
+    }
+
+    saveInJson(data) {
+        const path = ops.dirName + '/saved.json';
+        const json = JSON.stringify(data);
+        fs.writeFile(path, json, (err) => {
+            if (err) {
+                console.log('Err while writing file: ', err);
+            }
+            console.log('JSON saved');
+        });
+    }
+
+
+    finish() {
+        console.log(`Task finished.`);
+    }
+
+}
+
+const ops = {
+    dirName: './temp/LikedPhotos'
+};
+const data = require('./data/saved_data');
+const scrapper = new Scrapper(ops.dirName, data);
+
+scrapper.makeMagic();
